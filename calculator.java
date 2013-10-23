cad40aff17631e9cbc40c2abc13812885f1789a1
Phelonas <phelonas@ws-87-100.haw-1x.haw-hamburg.de>
2013-10-23 14:35:18 +0200

    subtraktion hinzugefügt

--- calculator.java
+++ calculator.java
@@ -2,7 +2,13 @@ import java.ulti.*;
 public class calculator{
 	public void main (String []args){
 		Scanner Scanner = new Scanner(System.in);
-		System.out.println("Bitte eingeben");
-		String next  = scanner.next();
+		System.out.println("Bitte eingeben [+/-] :");
+		String operation  = scanner.next();
+		int a= scanner.nextInt();
+		int b = scanner.nextInt();
+		if(operation.equals("-")){
+			int result = a-b;
+			System.out.println(a+"-"+ b+"="+result);
+		}
 	}
 }
