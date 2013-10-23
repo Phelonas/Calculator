5e422111621c7ea3fb4af88958fab17d960d5b06
Phelonas <phelonas@ws-87-100.haw-1x.haw-hamburg.de>
2013-10-23 14:14:38 +0200

    Revert 0b32924..86a7ff8
    This rolls back to commit 0b3292468aa59ef704b750f058b01ee0d1280fb3.

--- calculator.java
+++ calculator.java
@@ -2,7 +2,6 @@ import java.ulti.*;
 public class calculator{
 	public void main (String []args){
 		Scanner Scanner = new Scanner(System.in);
-		System.out.println("Bitte eingeben");
-		String next  = scanner.next();
+		
 	}
 }
